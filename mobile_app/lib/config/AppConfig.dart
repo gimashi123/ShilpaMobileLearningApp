7fa804b2f434a21b25163335e58bf92e02f97e6c
gimashi123 <gimashiishara2@gmail.com>
2026-03-09 12:34:38 +0530

    fix: missing api config

--- mobile_app/lib/config/AppConfig.dart
+++ mobile_app/lib/config/AppConfig.dart
@@ -4,4 +4,7 @@ class AppConfig {
   static String get apiBaseUrl {
     return dotenv.env['API_BASE_URL'] ?? 'http://192.168.1.100:3000';
   }
+  static String get apiChatBaseUrl {
+    return dotenv.env['API_CHAT_BASE_URL'] ?? 'http://192.168.1.100:8000';
+  }
 }
