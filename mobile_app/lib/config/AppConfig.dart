dc2f7d18cdb52641064c070a0f32fe10a51a4d9e
RavinduPerera <ravindus.creavint@gmail.com>
2026-03-14 10:53:39 +0530

    update: port changed

--- mobile_app/lib/config/AppConfig.dart
+++ mobile_app/lib/config/AppConfig.dart
@@ -2,9 +2,9 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class AppConfig {
   static String get apiBaseUrl {
-    return dotenv.env['API_BASE_URL'] ?? 'http://192.168.1.100:3000';
+    return dotenv.env['API_BASE_URL'] ?? 'http://192.168.8.179:3000';
   }
   static String get apiChatBaseUrl {
-    return dotenv.env['API_CHAT_BASE_URL'] ?? 'http://192.168.1.100:8000';
+    return dotenv.env['API_CHAT_BASE_URL'] ?? 'http://192.168.8.179:8000';
   }
 }
